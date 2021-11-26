7d0ea38f9e0c62ddc9911c872962405761d78900
nithyashanmugam82 <sachinshanmugamnithya@gmail.com>
2021-11-26 11:16:47 -0600

    Update stock-prediction.py

--- stocks/stock-prediction.py
+++ stocks/stock-prediction.py
@@ -9,71 +9,73 @@ from pyspark.sql.functions import col, from_json
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
 from pyspark.sql.types import StringType, StructType
-import sys
 sys.path.append("..")
 from lstm import LSTM 
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.model_selection import train_test_split
 
 spark = SparkSession.builder.appName("StockPrediction").config("spark.executor.memory", "70g").config("spark.driver.memory", "50g").config("spark.memory.offHeap.enabled",True).config("spark.memory.offHeap.size","16g").config("es.index.auto.create", "true").getOrCreate()
 spark.sparkContext.setLogLevel("ERROR")
 
 # Train and build the model if not available already
 def build_model(modelFile, inputFile):
-    df = spark.read.option("inferSchema", "true").option("header", "true").csv(inputFile)
+    df = spark.read.option("inferSchema", "true").option("header", "true").csv(inputFile)    
     
-    opscaler = MinMaxScaler()
-    ipscaler = MinMaxScaler()
-    inputs=ip.copy()
-    inputs.drop("Date", axis=1, inplace=True)
+    # opscaler = MinMaxScaler()
+    # ipscaler = MinMaxScaler()
+    # inputs=ip.copy()
+    # inputs.drop("Date", axis=1, inplace=True)
 
-    targets = inputs.filter(["Open"], axis=1)
-    targets.columns = ['target']
-    targets["target"]=targets['target'][1:].reset_index(drop=True)
-    targets.iloc[-1]['target'] = targets.iloc[:-1]['target'].mean()
+    # targets = inputs.filter(["Open"], axis=1)
+    # targets.columns = ['target']
+    # targets["target"]=targets['target'][1:].reset_index(drop=True)
+    # targets.iloc[-1]['target'] = targets.iloc[:-1]['target'].mean()
 
-    inputs[['Open','High','Low','Close','Volume','Trade_count','vwap']] = ipscaler.fit_transform(inputs[['Open','High','Low','Close','Volume','Trade_count','vwap']])
-    targets[['target']] = opscaler.fit_transform(targets[['target']])
+    # inputs[['Open','High','Low','Close','Volume','Trade_count','vwap']] = ipscaler.fit_transform(inputs[['Open','High','Low','Close','Volume','Trade_count','vwap']])
+    # targets[['target']] = opscaler.fit_transform(targets[['target']])
     
-    intrain, intest, optrain, optest = train_test_split(inputs, targets, test_size=0.2, shuffle=False)
+    # intrain, intest, optrain, optest = train_test_split(inputs, targets, test_size=0.2, shuffle=False)
     
-    lstm = LSTM(train_data=intrain, targets=optrain, batch_size=200, debug=0, test=0)
-    lstm.train(epoch=2, lr=1)
+    # lstm = LSTM(train_data=intrain, targets=optrain, batch_size=200, debug=0, test=0)
+    # lstm.train(epoch=2, lr=1)
     
-    lstm.goValidate(iptest, optest, opscaler, ipscaler)    
+    # lstm.goValidate(iptest, optest, opscaler, ipscaler)    
         
+    # # Save the trained model to disk.
+    # lstm.write().overwrite().save(modelFile)
+
     # Save the trained model to disk.
-    lstm.write().overwrite().save(modelFile)
+    #cvModel.write().overwrite().save(modelFile)
 
 # Load the model from the local file system to save on time. 
 def loadModel(modelFile):
     return CrossValidatorModel.read().load(modelFile)
 
-# Find the sentiment from the trained model and write the sentiment to Kafka topic
+# Find the predicted price from the trained model and write the predicted price to Kafka topic
 # This topic would be parsed by logstash and Kibana to show the analysis.
 def kafka_setup(trainedModel, kafkaListenServer, kafkaWriteServer, listenTopic, writeTopic):
     print(writeTopic)
     
     # This function would send the Close Price to the trained model to find the predicted price.
     def prdeictStockPrice(cvModel):        
-        converted = converter.transform(prediction)
+        # prediction = trainedModel.transform(cvModel)
+        # converter = IndexToString(inputCol="prediction", outputCol="PredictedPrice", labels=labels)
+        # converted = converter.transform(prediction)
         selected = converted.select("Close", "PredictedPrice").withColumnRenamed("PredictedPrice","value")
         selected.writeStream.format("kafka").option("kafka.bootstrap.servers", kafkaWriteServer).option('topic', writeTopic).start()
         spark.streams.awaitAnyTermination()
 
-    # The tweet text is read here from the Kafka topic to which the tweet json was sent by the twitterConnect API
+    # The Close Price is read here from the Kafka topic to which the alpaca json was sent by the alpaca API
 
-    schema = StructType().add("Date", StringType()).add("Open", StringType()).add("High", StringType()).add("Close", StringType()).add("Volume", StringType()).add("vwap", StringType())
+    schema = StructType().add("Date", StringType()).add("Open", StringType()).add("Low", StringType()).add("High", StringType()).add("Close", StringType()).add("Volume", StringType()).add("vwap", StringType()).add("Trade_count", StringType())
     # Create DataSet representing the stream of input lines from kafka
     lines = spark.readStream.format("kafka").option("kafka.bootstrap.servers", kafkaListenServer).option('subscribe', listenTopic).load().select(from_json(col("value").cast("string"), schema).alias("pdata"))
     closePrice = lines.select(col("pdata.Close").alias('Close'))
     prdeictStockPrice(closePrice)
 
 def main():
-    parser = argparse.ArgumentParser(description="Run Application to see Tweet Sentiments")
-    parser.add_argument('-m','--modelFile', help='Path on local drive where to the save trained Model if -b is enabled or just load the trained model from', default='/home/bijpraka/spark/assg3/nnmodel')
+    parser = argparse.ArgumentParser(description="Run Application to see Predicted Price for the stocks")
+    parser.add_argument('-m','--modelFile', help='Path on local drive where to the save trained Model if -b is enabled or just load the trained model from', default='/home/nithyashanmugam/TradeApp/nnmodel')
     parser.add_argument('-b','--buildModel', help='Option to specify to build the model and path on local drive to CSV File. If not enabled then use the trained model stored in -m', action='store_true')
-    parser.add_argument('-c','--csvFile', help='Path to CSV File where training data is saved. This would be used if -b is enabled', default="/home/bijpraka/spark/assg3/Tweets.csv")
+    parser.add_argument('-c','--csvFile', help='Path to CSV File where training data is saved. This would be used if -b is enabled', default="/home/nithyashanmugam/TradeApp/apple_5Min_data.csv")
     parser.add_argument('-kl','--kafkaListenServer', help='Kafka Server:PORT from where we are listening for data', default="localhost:9092")
     parser.add_argument('-kw','--kafkaWriteServer', help='Kafka Server:PORT to where we are writing our data', default="localhost:9092")
     parser.add_argument('-tl','--listenTopic', help='Kafka Topic to Listen to', default="messages")
